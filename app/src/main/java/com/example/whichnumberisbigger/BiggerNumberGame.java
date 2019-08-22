e6ca1c365d15e59dc84ba1d08ce536dad68916ec
JacksonMcLane <jackson.mclane20@gmail.com>
2019-08-22 13:56:39 -0700

    created BiggerNumberGame POJO    made generateRandomNumbers() and checkAnswer() methods

---
+++ app/src/main/java/com/example/whichnumberisbigger/BiggerNumberGame.java
@@ -0,0 +1,75 @@
+package com.example.whichnumberisbigger;
+
+public class BiggerNumberGame {
+    private int numberL, numberR;
+    private int score;
+    private int upperLimit, lowerLimit;
+
+    public BiggerNumberGame(int lowerLimit, int upperLimit) {
+        this.upperLimit = upperLimit;
+        this.lowerLimit = lowerLimit;
+    }
+
+
+    public void generateRandomNumbers() {
+        // generate a number between upper and lower limit -  inclusive
+            //store in numberL
+            //store another number that is different, store in numberR
+        numberL = (int)(Math.random()*(upperLimit-lowerLimit+1) + lowerLimit);
+        do{numberR = (int)(Math.random()*(upperLimit-lowerLimit+1) + lowerLimit);}
+            while (numberR == numberL);
+
+    }
+
+    public String checkAnswer(int answer) {
+        //determine if answer is right and return a relevant message
+            // based on the values of numberLeft & numberRight
+        int correctAnswer = Math.max(numberL, numberR);
+        if(answer == correctAnswer) {
+            return "Correct!";
+        }
+        else {
+            return "Wrong!";
+        }
+    }
+
+    public int getNumberLeft() {
+        return numberL;
+    }
+
+    public void setNumberLeft(int numberLeft) {
+        this.numberL = numberLeft;
+    }
+
+    public int getNumberRight() {
+        return numberR;
+    }
+
+    public void setNumberRight(int numberRight) {
+        this.numberR = numberRight;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getUpperLimit() {
+        return upperLimit;
+    }
+
+    public void setUpperLimit(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
+    public int getLowerLimit() {
+        return lowerLimit;
+    }
+
+    public void setLowerLimit(int lowerLimit) {
+        this.lowerLimit = lowerLimit;
+    }
+}
